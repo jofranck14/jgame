541b33ba37bcaf09a71747f146c5b19e574c5454
jofranck14 <absolujofranck@gmail.com>
2026-05-07 21:57:46 +0200

    version posgress

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,10 +8,10 @@ const pool = new Pool({
   password: env.DB_PASSWORD,
   database: env.DB_NAME,
   max:      env.DB_CONNECTION_LIMIT,
-  ssl:      { rejectUnauthorized: false }, // ← obligatoire pour Supabase
+  ssl:      { rejectUnauthorized: false },
+  family:   4, // ← Force IPv4
 });
 
-// Adaptateur pour garder la syntaxe mysql2 (.execute = .query en pg)
 pool.execute = (sql, params) => pool.query(sql, params);
 
 module.exports = { pool };
